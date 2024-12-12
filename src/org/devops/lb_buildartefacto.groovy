1d39b99b940bcd4b50b18e513f3587bdb8f49555
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-12 16:49:59 -0500

    pipelines
    carga de pipelines

---
+++ src/org/devops/lb_buildartefacto.groovy
@@ -0,0 +1,17 @@
+package org.devops
+
+def clone() {
+    git branch: "${env.nameBranch}", url: "${env.UrlGitHub}"
+}
+
+def install() {
+    sh 'npm install'
+}
+
+def build() {
+    sh 'npm run build'
+}
+
+def generateArtefact() {
+    sh 'tar -czvf artefacto.tar.gz ./build'
+}
