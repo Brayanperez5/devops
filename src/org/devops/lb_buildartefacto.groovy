f90c05f525251263b74bbe7f2b772df1d900b8e8
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-13 16:43:10 -0500

    prueba43

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -1,10 +1,13 @@
-package org.devops
-
-// Función para clonar el repositorio
-def clone() {
-    echo "Clonando el repositorio desde: ${env.GIT_URL_1}"
-    git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
-}
-def install() {
-    sh 'npm install'
+pipeline {
+    agent any
+    stages {
+        stage {
+            steps {
+                script {
+                        echo "Clonando el repositorio desde: ${env.GIT_URL_1}"
+                        git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
+                }
+            }
+        }
+    }
 }
