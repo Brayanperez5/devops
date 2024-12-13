fc2623b5d8b3f90b0b713e73df78f5b62072a3cb
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-13 16:49:15 -0500

    prueba39

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,47 +1,50 @@
-def lb_buildartefacto
-def lb_analisissonarqube
 def call(Map config) {
-node {
-    try {
-        // Cargar las librerías desde la ruta correcta
-        stage('Cargar Librerías') {
-            script {
-                echo "Cargando las librerías..."
-                lb_buildartefacto = load '../src/org/devops/lb_buildartefacto.groovy'
-                lb_analisissonarqube = load '../src/org/devops/lb_analisissonarqube.groovy'
+    pipeline {
+        agent any 
+        tools {
+            nodejs 'NodeJS' 
+        }
+        stages {
+            stage('Clonar repositorio') {
+                steps {
+                    script {
+                         org.devops.lb_buildartefacto.build()
+                    }
+                }
+            }
+            stage('Instalar dependencias') {
+                steps {
+                    script {
+                        sh 'npm install'
+                    }
+                }
+            }
+            stage('Run Tests and Coverage') {
+                steps {
+                    script {
+                        sh 'npm test'
+                    }
+                }
+            }
+            stage('SonarQube Analysis') {
+                steps {
+                    script {
+                        echo "Iniciando análisis con SonarQube..."
+                        org.devops.lb_analisissonarqube.analisisSonar(env.GIT_BRANCH_1)
+                    }
+                }
             }
         }
-        stage('Clonar Repositorio') {
-            script {
-                lb_buildartefacto.clone()
+        post {
+            always {
+                echo "Pipeline finalizado."
+            }
+            success {
+                echo "Pipeline ejecutado correctamente."
+            }
+            failure {
+                echo "El pipeline falló. Revisar los logs."
             }
         }
-        // Ejecutar las etapas utilizando las librerías cargadas
-        stage('Instalar Dependencias') {
-            script {
-                echo "Instalando dependencias..."
-                lb_buildartefacto.install() // Llamar la función desde la librería
-            }
-        }
-
-        stage('Ejecutar Pruebas') {
-            script {
-                echo "Ejecutando pruebas..."
-                lb_analisissonarqube.testCoverage() // Llamar la función desde la librería
-            }
-        }
-
-        stage('SonarQube Analysis') {
-            script {
-                echo "Iniciando análisis con SonarQube..."
-                lb_analisissonarqube.analisisSonar(env.GIT_BRANCH_1) // Llamar a SonarQube
-            }
-        }
-    } catch (Exception e) {
-        currentBuild.result = 'FAILURE'
-        throw e
-    } finally {
-        echo "Pipeline completado."
     }
-}
 }
