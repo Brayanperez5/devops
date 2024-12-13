12832959384b49cfa92625045acf1da41d4a3677
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-13 15:31:26 -0500

    prueba17

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -1,10 +1,10 @@
-package org.devops
-
-def clone() {
-    script {
-        git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
+pipeline {
+    def clone() {
+        script {
+            git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
+        }
     }
-}
-def install() {
-    sh 'npm install --verbose'
+    def install() {
+        sh 'npm install --verbose'
+    }
 }
