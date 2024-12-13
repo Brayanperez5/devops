8ae4da8ccd4bc5079e09a445419e384f799f258f
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-13 10:09:38 -0500

    a ver si sirve este p6

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,32 +1,14 @@
-def call(Map config) {
     pipeline {
-        agent any // Puede ejecutarse en cualquier agente disponible
+        agent any 
 
         tools {
-            nodejs 'NodeJS' // Asegúrate de tener NodeJS configurado en Jenkins
+            nodejs 'NodeJS' 
         }
 
         stages {
-            stage('Inicia el environment') {
-                steps {
-                    script {
-                        // Calcular y asignar variables de entorno
-                        env.PROJECT_NAME = config.repoUrl.tokenize('/').last().replace('.git', '')
-                        env.GIT_BRANCH_1 = config.branch ?: 'main'
-                        env.GIT_URL_1 = config.repoUrl ?: 'src'
-
-                        echo "Variables de entorno inicializadas:"
-                        echo "PROJECT_NAME: ${env.PROJECT_NAME}"
-                        echo "GIT_BRANCH: ${env.GIT_BRANCH_1}"
-                        echo "GIT_REPO_URL: ${env.GIT_URL_1}"
-                    }
-                }
-            }
-
             stage('Clonar el repositorio') {
                 steps {
                     script {
-                        echo "Clonando el repositorio: ${env.GIT_URL_1}"
                         org.devops.lb_buildartefacto.clone()
                     }
                 }
@@ -35,7 +17,6 @@ def call(Map config) {
             stage('Instalar depencias') {
                 steps {
                     script {
-                        echo "Instalando dependencias..."
                         org.devops.lb_buildartefacto.install()
                     }
                 }
@@ -44,7 +25,6 @@ def call(Map config) {
             stage('Testeando') {
                 steps {
                     script {
-                        echo "Ejecutando pruebas y generando cobertura..."
                         org.devops.lb_analisissonarqube.testCoverage()
                     }
                 }
@@ -53,7 +33,6 @@ def call(Map config) {
             stage('Analisar en sonarqube') {
                 steps {
                     script {
-                        echo "Iniciando análisis con SonarQube..."
                         org.devops.lb_analisissonarqube.analisisSonar(env.GIT_URL_1)
                     }
                 }
@@ -72,4 +51,3 @@ def call(Map config) {
             }
         }
     }
-}
