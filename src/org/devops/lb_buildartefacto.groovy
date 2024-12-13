6c2504e5d60d3d720995f0d3754b302b314454d5
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-13 16:48:17 -0500

    prueba38

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -1,8 +1,6 @@
 package org.devops
 
 def clone() {
-    script {
-        echo "Clonando el repositorio desde: ${env.GIT_URL_1}"
-        git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
-    }
+    echo "Clonando el repositorio desde: ${env.GIT_URL_1}"
+    git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
 }
