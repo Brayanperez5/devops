cbe25cf64962883f5321b7918ea661d2a0aef022
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-17 09:38:51 -0500

    prueba 61

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -4,3 +4,11 @@ def clone() {
     echo "Clonando el repositorio desde: ${env.GIT_URL_1}"
     git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
 }
+
+def build() {
+    sh 'npm install'
+    sh 'npm run build'
+}
+def generarArtefacto() {
+    archiveArtifacts artifacts: '**/dist/**/*.*', fingerprint: true
+}
