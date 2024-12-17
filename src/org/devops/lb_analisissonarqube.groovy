5554a21d85ffab1c791b46636c7e2b1d3b6770e9
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-17 11:41:37 -0500

    prueba 63

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -7,10 +7,10 @@ def testCoverage() {
 
 def analisisSonar(gitName) {
     echo "Iniciando análisis con SonarQube..."
-    def scannerHome = tool 'sonarScannerJenkins'  
+    def scannerHome = tool 'sonar-scanner'  
     if(scannerHome) {
-        withSonarQubeEnv('sonarScannerJenkins') {  
-            sh "${scannerHome}/bin/sonarScannerJenkins -Dsonar.projectKey=${gitName} -Dsonar.projectName=${gitName} -Dsonar.sources=. -Dsonar.tests=src/__test__ -Dsonar.exclusions='**/*.test.js' -Dsonar.testExecutionReportPaths=./test-report.xml -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
+        withSonarQubeEnv('sonar-scanner') {  
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${gitName} -Dsonar.projectName=${gitName} -Dsonar.sources=. -Dsonar.tests=src/__test__ -Dsonar.exclusions='**/*.test.js' -Dsonar.testExecutionReportPaths=./test-report.xml -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
         }
     } else {
         error 'SonarQube Scanner not found'
