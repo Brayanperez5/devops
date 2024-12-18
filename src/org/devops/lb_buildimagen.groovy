24b3208f154f067b98841e620c3907bdebe06154
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-18 16:57:06 -0500

    jenkins fase 2 prueba 15

--- src/org/devops/lb_buildimagen.groovy
+++ src/org/devops/lb_buildimagen.groovy
@@ -1,5 +1,5 @@
 package org.devops
 
 def buildImageDocker(projectGitName) {
-    bat "docker build -t brayanperez55/${projectGitName} ."
+    sh "docker build -t brayanperez55/${projectGitName} ."
 }
