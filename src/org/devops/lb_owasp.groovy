c2114ef423d6c13f700125a9209261e8a603129f
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-19 16:07:12 -0500

    cambio prueba 3

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -2,8 +2,9 @@ package org.devops
 
 def analisisOwasp(projectGitName) {
     sh """ 
-    docker run --rm -v projectOwasp:/zap/wrk/:rw \
-    --user root --network=${env.NameNetwork} \
+    docker run -d --name owasp-analysis \
+    -v projectOwasp:/zap/wrk/:rw \
+    --user zap --network=${env.NameNetwork} \
     -p 8081:8080 -p 8090:8090 -i edansama96/zap2docker-stable \
     zap-webswing.sh -t ${env.dominio} -r ProjectOwasp.html -I
     """
