e1b386f4b228a0f7c8a518c05679bc1e6fbf53c2
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-19 16:53:50 -0500

    cambio prueba 6

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,20 +1,11 @@
 package org.devops
 
-def analisisOwasp(projectGitName) {
+def analisisOwasp(projectGitName){
     sh """ 
-    docker run -d --name owasp-analysis \
-    -v projectOwasp:/zap/wrk/:rw \
-    --user zap --network=${env.NameNetwork} \
-    -p 8081:8080 -p 8090:8090 -i edansama96/zap2docker-stable \
-    zap-webswing.sh -t ${env.dominio} -r /zap/wrk/ProjectOwasp.html -I
+        docker run -d --name analisis-owasp \
+        -v projectOwasp:/zap/wrk/:rw \
+        --user root --network=${env.NameNetwork} \
+        -p 8081:8080 -p 8090:8090 -i edansama96/zap2docker-stable \
+        zap-webswing.sh -t ${env.dominio} -r ProjectOwasp.html -I
     """
-    // Verificar si el reporte fue generado
-    sh """
-    if [ ! -f /zap/wrk/ProjectOwasp.html ]; then
-        echo "El reporte no fue generado. Revisa los parámetros o el volumen."
-        docker logs owasp-analysis
-        exit 1
-    fi
-    """
-    echo "Reporte generado correctamente en el volumen projectOwasp."
 }
