8f21a0a2d9f27b0c8163a48afe047de2b511d21b
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-12-19 19:25:15 -0500

    cambio prueba 11

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,14 +1,12 @@
 package org.devops
 
-def analisisOwasp(projectGitName) {
-    sh """
-    docker run --rm \
-        -v /path/to/local/ProjectOwasp:/zap/wrk/:rw \
-        --user root \
-        --network=${env.NameNetwork} \
-        edansama96/zap2docker-stable \
-        zap-full-scan.py \
-        -t http://${env.dominio} \
-        -r /zap/wrk/ProjectOwasp.html -I
-    """
+def analisisOwasp(projectGitName){
+    //Ejecutar el análisis OWASP con ZAP, asegurando volumen y variables correctas
+    sh """ docker run --rm -v projectOwasp:/zap/wrk/:rw \
+          --user root --network=${env.NameNetwork} \
+          -t edansama96/zap2docker-stable \
+           zap-full-scan.py \
+          -t http://${env.dominio} \
+           -r ProjectOwasp.html -I
+       """
 }
